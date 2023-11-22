22ced50e40268373c0591c21da6b2a65a16fdc44
EAG <pablo.alcalde.garcia@gmail.com>
2023-11-22 19:23:29 +0100

    asdf

--- app/src/main/java/com/example/entregaclasedi_tema2/Ejercicio2.kt
+++ app/src/main/java/com/example/entregaclasedi_tema2/Ejercicio2.kt
@@ -1,5 +1,6 @@
 package com.example.entregaclasedi_tema2
 
+import android.content.Intent
 import android.icu.text.CompactDecimalFormat.CompactStyle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,28 +17,29 @@ class Ejercicio2 : AppCompatActivity() {
     }
 
     fun boton(view: View) {
-
-        val usuario = findViewById<TextInputEditText>(R.id.layoutUsuarioText)
-        val usuario2 = usuario.text ?: ""
-        val contrasena = findViewById<TextInputEditText>(R.id.layoutContrasenaText)
-        val gmail = findViewById<TextInputEditText>(R.id.layoutEmailText)
-        val gmail2 = gmail.text ?: ""
-
-
-        if (usuario2.isEmpty()) {
-            usuario.error = "Error al introducir usuario"
-            usuario.setBackgroundColor(getColor(R.color.rojoclaro))
-            usuario.setTextColor(getColor(R.color.white))
-            usuario.setText("GAY69")
-        } else if (contrasena.length() < 6) {
-            contrasena.error = "Contraseña incorrecta."
-            contrasena.setBackgroundColor(getColor(R.color.rojoclaro))
-            contrasena.setTextColor(getColor(R.color.white))
-        } else if (!gmail2.contains("@") && !gmail2.contains(".")) {
-            gmail.error = "Correo electronico no valido."
-            gmail.setBackgroundColor(getColor(R.color.rojoclaro))
-            gmail.setTextColor(getColor(R.color.white))
-            gmail.setText("GAY69ChupaPitos@gmail.com")
-        }
-    }
+        val intent = Intent(this, Ejercicio3::class.java)
+        startActivity(intent)
+//        val usuario = findViewById<TextInputEditText>(R.id.layoutUsuarioText)
+//        val usuario2 = usuario.text ?: ""
+//        val contrasena = findViewById<TextInputEditText>(R.id.layoutContrasenaText)
+//        val gmail = findViewById<TextInputEditText>(R.id.layoutEmailText)
+//        val gmail2 = gmail.text ?: ""
+//
+//
+//        if (usuario2.isEmpty()) {
+//            usuario.error = "Error al introducir usuario"
+//            usuario.setBackgroundColor(getColor(R.color.rojoclaro))
+//            usuario.setTextColor(getColor(R.color.white))
+//            usuario.setText("GAY69")
+//        } else if (contrasena.length() < 6) {
+//            contrasena.error = "Contraseña incorrecta."
+//            contrasena.setBackgroundColor(getColor(R.color.rojoclaro))
+//            contrasena.setTextColor(getColor(R.color.white))
+//        } else if (!gmail2.contains("@") && !gmail2.contains(".")) {
+//            gmail.error = "Correo electronico no valido."
+//            gmail.setBackgroundColor(getColor(R.color.rojoclaro))
+//            gmail.setTextColor(getColor(R.color.white))
+//            gmail.setText("GAY69ChupaPitos@gmail.com")
+//        }
+   }
 }
--- app/src/main/java/com/example/entregaclasedi_tema2/Ejercicio3.kt
+++ app/src/main/java/com/example/entregaclasedi_tema2/Ejercicio3.kt
@@ -2,19 +2,87 @@ package com.example.entregaclasedi_tema2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
+import java.util.Calendar
 
 class Ejercicio3 : AppCompatActivity() {
+    lateinit var correo: TextInputEditText
+    lateinit var contrasena: TextInputEditText
+    lateinit var nombre: TextInputEditText
+    lateinit var cp: TextInputEditText
+    lateinit var fecha_nacimiento:TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio3)
 
-        var correo = findViewById<TextInputEditText>(R.id.correo)
+        correo = findViewById<TextInputEditText>(R.id.correo)
+        contrasena = findViewById<TextInputEditText>(R.id.contrasena)
+        nombre = findViewById(R.id.nombre)
+        cp= findViewById(R.id.cp)
+        fecha_nacimiento= findViewById(R.id.fecha_nacimiento)
 
+        correo.addTextChangedListener {
+            if (it.isNullOrBlank() || !it.contains("@") || !it.contains(".")) {
+                correo.error = "Correo no completado con exito"
+            } else {
+                contrasena.isFocusable = true
+            }
+        }
+
+            contrasena.addTextChangedListener {
+                println(it)
+                if (it != null) {
+                    if (it.length < 7) {
+                        contrasena.error = "La contraseña es demasiado corta"
+                    }
+                    val regex = Regex("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).+\$")
+
+                    if (it.matches(regex)) {
+                        println("entra")
+                        nombre.isFocusable = true
+
+                    } else {
+                        contrasena.error =
+                            ("La contraseña debe contener mayusc. minusc. y numeros ")
+                    }
+                } else {
+                    contrasena.error = "La contraseña no puede estar vacia"
+                }
+
+
+            }
+            nombre.addTextChangedListener{
+                if(it.isNullOrBlank()){
+                    nombre.error="Esque no tienes nombre? Gilipollas"
+                    cp.isFocusable=false
+                    Log.v("focusable",cp.focusable.toString())
+
+                }else{
+                    cp.isFocusable=true
+                    Log.v("focusable",cp.focusable.toString())
+                }
+            }
+
+            cp.addTextChangedListener {
+                if (it.toString().length < 5 && it.toString().length>0) {
+                    cp.error = "El CP es incorrecto"
+                } else {
+                    fecha_nacimiento.isFocusable = true
+                }
+            }
+
+            fecha_nacimiento.addTextChangedListener{
+                var ano_actual = Calendar.YEAR
+
+            }
+
+            fun botonE3(view: View) {
+
+            }
+        }
     }
-
-    fun botonE3(view: View) {
-
-    }
-}
